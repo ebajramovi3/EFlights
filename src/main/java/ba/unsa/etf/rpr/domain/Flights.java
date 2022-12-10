b68cb03cc73bbdf432ef1bfa2e86cb8a16a27592
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-10 19:11:01 +0100

    add attributes for 'Flights'

--- src/main/java/ba/unsa/etf/rpr/domain/Flights.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Flights.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Date;
+
 public class Flights {
+    private int id;
+    private String nameOfAirline;
+    private int maxNumberOfPassengers;
+    private Destinations destination;
+    private Persons pilot;
+    private Date dateOfDeparture;
+
+
 }
