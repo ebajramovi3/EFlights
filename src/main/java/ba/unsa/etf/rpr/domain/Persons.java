d18837a2489e8a8828cb4675ac62f48dafd6f1fa
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-10 19:21:32 +0100

    'Persons' attributes

--- src/main/java/ba/unsa/etf/rpr/domain/Persons.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Persons.java
@@ -1,4 +1,10 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Date;
+
 public class Persons {
+    private String passportId, firstName, lastName;
+    private Flights destination;
+    private String citizenship;
+    private Date dateOfBirth;
 }
