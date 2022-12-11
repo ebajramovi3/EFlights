1799fbfbfd1e658a5bdac6673ebaa9271c9a8767
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-11 18:59:46 +0100

    add flights dao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/FlightsDAO.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Flights;
+
+import java.util.Date;
+import java.util.List;
+
+public interface FlightsDAO<T> extends DAO {
+    List<Flights> searchByDate(Date dateOfFlight);
+
+    List<Flights> searchByAirline(String nameOfAirline);
+
+    List<Flights> searchById(int id);
+}
