331f2a46425d483f65f33b19938c14398066af8f
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-11 19:58:49 +0100

    change destinations dao

--- src/main/java/ba/unsa/etf/rpr/dao/DestinationsDAO.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DestinationsDAO.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Destinations;
 import java.util.List;
 
-public interface DestinationsDAO<T> extends DAO<Destinations, Integer>{
+public interface DestinationsDAO extends DAO<Destinations, Integer>{
     List<Destinations> searchByCity(String nameOfCity);
 
     List<Destinations> searchByCountry(String nameOfCountry);
