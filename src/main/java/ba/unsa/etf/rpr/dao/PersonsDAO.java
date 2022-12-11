c500a657d5daea35af8ce9fcc2e5cfc078dea733
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-11 19:04:23 +0100

    add persons dao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/PersonsDAO.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Persons;
+
+import java.util.List;
+
+public interface PersonsDAO<T> extends DAO {
+    List<Persons> searchByPassportId(String passportId);
+
+    List<Persons> searchByFirstName(String firstName);
+
+    List<Persons> searchByLastName(String lastName);
+
+    List<Persons> searchByCitizenship(String citizenship);
+}
