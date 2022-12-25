bbc16b288f29824513bb9cd10c7259bf9270be30
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-25 18:46:31 +0100

    add setters and getters for departure

--- src/main/java/ba/unsa/etf/rpr/domain/Departure.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Departure.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * @author Esma Bajramović
@@ -10,5 +11,58 @@ public class Departure {
     private String city, country;
     private Date dateOfDeparture;
 
+    public Departure(int departureId, String city, String country, Date dateOfDeparture) {
+        this.departureId = departureId;
+        this.city = city;
+        this.country = country;
+        this.dateOfDeparture = dateOfDeparture;
+    }
 
+    public Departure() {
+    }
+
+    public int getDepartureId() {
+        return departureId;
+    }
+
+    public void setDepartureId(int departureId) {
+        this.departureId = departureId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getDateOfDeparture() {
+        return dateOfDeparture;
+    }
+
+    public void setDateOfDeparture(Date dateOfDeparture) {
+        this.dateOfDeparture = dateOfDeparture;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Departure departure = (Departure) o;
+        return departureId == departure.departureId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departureId, city, country, dateOfDeparture);
+    }
 }
