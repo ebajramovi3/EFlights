61f15f6504e5fc1c9a49b36eb17389308931ab3f
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-25 22:14:20 +0100

    departure SQLimpl getByCountry

--- src/main/java/ba/unsa/etf/rpr/dao/DepartureDaoImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DepartureDaoImplementation.java
@@ -167,7 +167,30 @@ public class DepartureDaoImplementation extends SQLConnection implements Departu
      */
     @Override
     public List<Departure> getByCountry(String countryOfDeparture) {
-        return null;
+        String query = "SELECT * FROM Departure WHERE country = ?";
+        try{
+            List<Departure> departures = new ArrayList<>();
+
+            PreparedStatement statement = this.connection.prepareStatement(query);
+            statement.setString(1, countryOfDeparture);
+
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                Departure departure = new Departure();
+
+                departure.setDepartureId(rs.getInt("id"));
+                departure.setCountry(rs.getString("country"));
+                departure.setCity("city");
+                departure.setDateOfDeparture(rs.getDate("date"));
+                departure.setArrival(new ArrivalDaoImplementation().getById(rs.getInt("arrivalId")));
+
+                departures.add(departure);
+            }
+            rs.close();
+            return departures;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
