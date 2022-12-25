196686d0a3fcfcc1e0cb1e5747704adfc63ef03f
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-25 23:30:37 +0100

    sql connection

--- src/main/java/ba/unsa/etf/rpr/dao/SQLConnection.java
+++ src/main/java/ba/unsa/etf/rpr/dao/SQLConnection.java
@@ -1,14 +1,21 @@
 package ba.unsa.etf.rpr.dao;
 
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class SQLConnection {
     protected Connection connection;
 
     SQLConnection(){
+
         try{
-            this.connection = DriverManager.getConnection("url", "name", "password");
+            FileReader reader = new FileReader("db.properties");
+
+            Properties p = new Properties();
+            p.load(reader);
+            this.connection = DriverManager.getConnection(p.getProperty("link"), p.getProperty("user"), p.getProperty("password"));
         }catch (Exception exception){
             System.out.println("Connection with SQL failed.");
         }
