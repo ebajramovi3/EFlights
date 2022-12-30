9be9e4f04a17e84fdf3190e3deec8166db1a2432
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-30 18:19:36 +0100

    add home.fxml and homeController

---
+++ src/main/java/ba/unsa/etf/rpr/conrollers/HomeController.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.conrollers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+
+public class HomeController {
+    public Hyperlink LogInId;
+    public Button ArrivalId;
+    public Button DepartireId;
+    public Button SearchFlightsId;
+    public Button CheckInId;
+
+    public HomeController() {
+    }
+
+    @FXML
+    public void initilize(){
+
+    }
+}
