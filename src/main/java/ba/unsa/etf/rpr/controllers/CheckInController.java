4764de6b1277be188aef630ee6f7a38dd8b7fb2b
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-31 21:01:34 +0100

    add CheckInController

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/CheckInController.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class CheckInController {
+    public TextField passportId;
+    public TextField flightNumberId;
+    public TextField firstNameId;
+    public TextField lastNameId;
+    public TextField CitizenshipId;
+    public DatePicker dateId;
+    public Button OkButtonId;
+
+    public void okButtonAction(ActionEvent actionEvent) {
+    }
+
+
+}
