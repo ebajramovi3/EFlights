01c3c14c98b13b79d6d3a9a58f9a13bb816c2c06
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-12-31 21:02:26 +0100

    add DepartureController

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/DepartureController.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class DepartureController {
+    public TableView tableId;
+    public TableColumn airlineId;
+    public TableColumn destinationId;
+    public TableColumn flightNumberId;
+    public TableColumn dateId;
+    public TextField searchFlightsId;
+}
