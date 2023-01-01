59f10ddb43d797032a359ef33f2e1af96766cc3f
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-01-01 23:06:39 +0100

    add methods add and getAll

--- src/main/java/ba/unsa/etf/rpr/business/FlightsManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/FlightsManager.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.Arrival;
+import ba.unsa.etf.rpr.domain.Flights;
 import ba.unsa.etf.rpr.exceptions.FlightsException;
 import ba.unsa.etf.rpr.dao.DaoFactory;
+
+import java.util.List;
 
 public class FlightsManager {
     public void delete(int categoryId) throws FlightsException {
@@ -15,5 +19,18 @@ public class FlightsManager {
         }
     }
 
+    public List<Flights> getAll() throws FlightsException{
+        return DaoFactory.flightsDao().getAll();
+    }
 
+    public Flights add(Flights flight) throws FlightsException{
+        try{
+            return DaoFactory.flightsDao().add(flight);
+        }catch (Exception e){
+            if(e.getMessage().contains("UQ_NAME")){
+                throw new FlightsException(e.getMessage(), e);
+            }
+            throw e;
+        }
+    }
 }
