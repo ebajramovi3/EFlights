d79023d3aa9169d62f417bb788b65a1c1c137399
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-01-08 14:12:28 +0100

    add methods

--- src/main/java/ba/unsa/etf/rpr/controllers/ArrivalsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ArrivalsController.java
@@ -1,47 +1,40 @@
 package ba.unsa.etf.rpr.controllers;
 
-import ba.unsa.etf.rpr.business.ArrivalManager;
-import ba.unsa.etf.rpr.domain.Arrival;
-import ba.unsa.etf.rpr.domain.Departure;
+import ba.unsa.etf.rpr.business.FlightsManager;
 import ba.unsa.etf.rpr.domain.Flights;
 import ba.unsa.etf.rpr.exceptions.FlightsException;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class ArrivalsController {
-    private final ArrivalManager arrivalManager = new ArrivalManager();
+    private final FlightsManager arrivalManager = new FlightsManager();
 
     public TableView tableId;
     public TableColumn<Flights, String> airlineId;
-    public TableColumn<Departure, String> destinationId;
+    public TableColumn<Flights, String> destinationId;
     public TableColumn<Flights, Integer> flightNumberId;
-    public TableColumn<Departure, Date> dateId;
+    public TableColumn<Flights, Date> dateId;
     public TextField searchId;
 
     @FXML
     public void initialize(){
-        /*airlineId.setCellValueFactory(new PropertyValueFactory<Flights, String>("airline_name"));
-        destinationId.setCellValueFactory(new PropertyValueFactory<Departure, String>(""));
+        airlineId.setCellValueFactory(new PropertyValueFactory<Flights, String>("nameOfAirline"));
+        destinationId.setCellValueFactory(new PropertyValueFactory<Flights, String>("cityOfArrival"));
         flightNumberId.setCellValueFactory(new PropertyValueFactory<Flights, Integer>("id"));
-        dateId.setCellValueFactory(new PropertyValueFactory<Departure, Date>("date"));
+        dateId.setCellValueFactory(new PropertyValueFactory<Flights, Date>("date"));
 
         try{
-            List<Arrival> arrivals = arrivalManager.getAll();
-            airlineId.set)C
+            tableId.setItems(FXCollections.observableList(arrivalManager.searchArrivals("Sarajevo")));
             tableId.refresh();
         } catch (FlightsException exception){
 
-        }*/
+        }
     }
 
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/DepartureController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DepartureController.java
@@ -1,14 +1,39 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.FlightsManager;
+import ba.unsa.etf.rpr.domain.Flights;
+import ba.unsa.etf.rpr.exceptions.FlightsException;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Date;
 
 public class DepartureController {
+    FlightsManager departureManager = new FlightsManager();
+
     public TableView tableId;
-    public TableColumn airlineId;
-    public TableColumn destinationId;
-    public TableColumn flightNumberId;
-    public TableColumn dateId;
-    public TextField searchFlightsId;
+    public TableColumn<Flights, String> airlineId;
+    public TableColumn<Flights, String> destinationId;
+    public TableColumn<Flights, Integer> flightNumberId;
+    public TableColumn<Flights, Date> dateId;
+    public TextField searchId;
+
+    @FXML
+    public void initialize(){
+        airlineId.setCellValueFactory(new PropertyValueFactory<Flights, String>("nameOfAirline"));
+        destinationId.setCellValueFactory(new PropertyValueFactory<Flights, String>("cityOfDeparture"));
+        flightNumberId.setCellValueFactory(new PropertyValueFactory<Flights, Integer>("id"));
+        dateId.setCellValueFactory(new PropertyValueFactory<Flights, Date>("date"));
+
+        try{
+            tableId.setItems(FXCollections.observableList(departureManager.searchDepartures("Sarajevo")));
+            tableId.refresh();
+        } catch (FlightsException exception){
+
+        }
+    }
 }
