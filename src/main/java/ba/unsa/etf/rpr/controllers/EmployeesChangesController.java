6166088afa2884acb5bfa9558bd05e12e7fc6470
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-01-18 21:36:09 +0100

    add EmployeesChanges

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeesChangesController.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class EmployeesChangesController {
+    public ComboBox comboBoxId;
+    public TextField firstNameId;
+    public TextField lastNameId;
+    public TextField usernameId;
+    public PasswordField passwordId;
+    public Button OkButton;
+
+    public void okButtonAction(ActionEvent actionEvent) {
+
+    }
+
+    public void comboBoxAction(ActionEvent actionEvent) {
+    }
+}
