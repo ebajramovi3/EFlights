c08330be03c7d2f390e5880dc7729b36e41f99aa
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-01-18 21:36:34 +0100

    add FlightsChanges

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/FlightsChangesController.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class FlightsChangesController {
+    public ComboBox ComboBoxId;
+    public TextField flightId;
+    public TextField departureId;
+    public TextField arrivalId;
+    public TextField airlineId;
+    public DatePicker dateId;
+    public Button okButton;
+
+    public void okButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void comboBoxAction(ActionEvent actionEvent) {
+    }
+}
