6b8fdf69b1dc0eedf2440a92da4e5e3da88e1eaf
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-01-19 21:13:51 +0100

    add update flight

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/UpdateFlightController.java
@@ -0,0 +1,47 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.business.FlightsManager;
+import ba.unsa.etf.rpr.domain.Flights;
+import ba.unsa.etf.rpr.exceptions.FlightsException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class UpdateFlightController {
+    private Flights flights;
+    private final FlightsManager flightsManager = new FlightsManager();
+
+    public TextField flightId;
+    public TextField airlineId;
+    public TextField departureId;
+    public TextField arrivalId;
+    public DatePicker dateId;
+    public Button okButton;
+
+    public UpdateFlightController(Flights flights){
+        this.flights = flights;
+    }
+
+    @FXML
+    public void initialize(){
+        flightId.setText(String.valueOf(flights.getId()));
+        airlineId.setText(flights.getNameOfAirline());
+        departureId.setText(flights.getCityOfDeparture());
+        arrivalId.setText(flights.getCityOfArrival());
+        dateId.setValue(flights.getDate());
+    }
+
+
+    public void okButtonAction(ActionEvent actionEvent) {
+        try{
+            flights.setNameOfAirline(airlineId.getText());
+            flights.setCityOfDeparture(departureId.getText());
+            flights.setCityOfArrival(arrivalId.getText());
+            flights.setDate(dateId.getValue());
+            flightsManager.update(flights);
+            okButton.getScene().getWindow().hide();
+        }catch (FlightsException exception){
+            new Alert(Alert.AlertType.NONE, exception.getMessage(), ButtonType.OK).show();
+        }
+    }
+}
