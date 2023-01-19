fad6ed3d15d6befe1b612e04a076c921c966bfd5
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-01-19 19:02:23 +0100

    add initialize

--- src/main/java/ba/unsa/etf/rpr/controllers/FlightsChangesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/FlightsChangesController.java
@@ -1,23 +1,51 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.FlightsManager;
+import ba.unsa.etf.rpr.domain.Flights;
+import ba.unsa.etf.rpr.exceptions.FlightsException;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Date;
 
 public class FlightsChangesController {
-    public ComboBox ComboBoxId;
-    public TextField flightId;
-    public TextField departureId;
-    public TextField arrivalId;
-    public TextField airlineId;
-    public DatePicker dateId;
-    public Button okButton;
+    private final FlightsManager flightsManager = new FlightsManager();
 
-    public void okButtonAction(ActionEvent actionEvent) {
+    public TableView<Flights> tableId;
+    public TableColumn<Flights, String> airlineId;
+    public TableColumn<Flights, Integer> flightId;
+    public TableColumn<Flights, String> departureId;
+    public TableColumn<Flights, String> arrivalId;
+    public TableColumn<Flights, Date> dateId;
+    public Button addButton;
+    public Button updateButton;
+    public Button deleteButton;
+
+    public void addButtonAction(ActionEvent actionEvent) {
     }
 
-    public void comboBoxAction(ActionEvent actionEvent) {
+    public void updateButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteButtonAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void initialize(){
+        airlineId.setCellValueFactory(new PropertyValueFactory<Flights, String>("nameOfAirline"));
+        departureId.setCellValueFactory(new PropertyValueFactory<Flights, String>("cityOfDeparture"));
+        arrivalId.setCellValueFactory(new PropertyValueFactory<Flights, String>("CityOfArrival"));
+        flightId.setCellValueFactory(new PropertyValueFactory<Flights, Integer>("id"));
+        dateId.setCellValueFactory(new PropertyValueFactory<Flights, Date>("date"));
+
+        try{
+            tableId.setItems(FXCollections.observableList(flightsManager.getAll()));
+            tableId.refresh();
+        } catch (FlightsException exception){
+
+        }
     }
 }
