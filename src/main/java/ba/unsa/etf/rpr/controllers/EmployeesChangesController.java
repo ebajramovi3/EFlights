bf43f75fc5a9430f1a6c48e1f0a88eca5b3402a0
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-01-19 19:00:49 +0100

    add initialize

--- src/main/java/ba/unsa/etf/rpr/controllers/EmployeesChangesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeesChangesController.java
@@ -1,23 +1,50 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.EmployeeManager;
+import ba.unsa.etf.rpr.domain.Employees;
+import ba.unsa.etf.rpr.domain.Flights;
+import ba.unsa.etf.rpr.exceptions.FlightsException;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Date;
 
 public class EmployeesChangesController {
-    public ComboBox comboBoxId;
-    public TextField firstNameId;
-    public TextField lastNameId;
-    public TextField usernameId;
-    public PasswordField passwordId;
-    public Button OkButton;
+    private final EmployeeManager employeeManager = new EmployeeManager();
 
-    public void okButtonAction(ActionEvent actionEvent) {
+    public Button addButton;
+    public Button updateButton;
+    public Button deleteButton;
+    public TableView<Employees> tableId;
+    public TableColumn<Employees, Integer> employeeId;
+    public TableColumn<Employees, String> lastNameId;
+    public TableColumn<Employees, String> firstNameId;
+    public TableColumn<Employees, String> usernameId;
 
+    public void addButtonAction(ActionEvent actionEvent) {
     }
 
-    public void comboBoxAction(ActionEvent actionEvent) {
+    public void updateButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteButtonAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void initialize(){
+        employeeId.setCellValueFactory(new PropertyValueFactory<Employees, Integer>("id"));
+        lastNameId.setCellValueFactory(new PropertyValueFactory<Employees, String>("lastName"));
+        firstNameId.setCellValueFactory(new PropertyValueFactory<Employees, String>("firstName"));
+        usernameId.setCellValueFactory(new PropertyValueFactory<Employees, String>("username"));
+
+        try{
+            tableId.setItems(FXCollections.observableList(employeeManager.getAll()));
+            tableId.refresh();
+        } catch (FlightsException exception){
+            new Alert(Alert.AlertType.NONE, exception.getMessage(), ButtonType.OK).show();
+        }
     }
 }
