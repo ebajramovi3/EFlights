388cd7d74544018d33c493274132692faa267714
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-01-19 20:30:19 +0100

    add methods update and delete

--- src/main/java/ba/unsa/etf/rpr/controllers/EmployeesChangesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeesChangesController.java
@@ -2,15 +2,20 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.EmployeeManager;
 import ba.unsa.etf.rpr.domain.Employees;
-import ba.unsa.etf.rpr.domain.Flights;
 import ba.unsa.etf.rpr.exceptions.FlightsException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Date;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class EmployeesChangesController {
     private final EmployeeManager employeeManager = new EmployeeManager();
@@ -25,9 +30,32 @@ public class EmployeesChangesController {
     public TableColumn<Employees, String> usernameId;
 
     public void addButtonAction(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addEmployee.fxml"));
+        stage.setTitle("Add employee");
+        try {
+            stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+        tableId.refresh();
     }
 
     public void updateButtonAction(ActionEvent actionEvent) {
+        Employees employee = tableId.getSelectionModel().getSelectedItem();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/updateEmployee.fxml"));
+        UpdateEmployeeController controller = new UpdateEmployeeController(employee);
+        loader.setController(controller);
+        stage.setTitle("Update employee");
+        try {
+            stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+        tableId.refresh();
     }
 
     public void deleteButtonAction(ActionEvent actionEvent) {
@@ -38,6 +66,7 @@ public class EmployeesChangesController {
         } catch (FlightsException e) {
             throw new RuntimeException(e);
         }
+        tableId.refresh();
     }
 
     @FXML
