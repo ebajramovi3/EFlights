4d1a9af5ce813ba712c8ea1549fd67d875135358
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-02-05 11:50:34 +0100

    add App class

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,36 +1,101 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.controllers.HomeController;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import ba.unsa.etf.rpr.business.FlightsManager;
+import ba.unsa.etf.rpr.business.PersonsManager;
+import ba.unsa.etf.rpr.domain.Flights;
+import org.apache.commons.cli.*;
 
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
-public class App extends Application {
-    /*public static void main( String[] args )
-    {
-    }
+public class App {
+    /**
+     * Defining final variables to describe all code having options
+     */
+    private static final Option arrivals = new Option("a","get-arrivals",false, "Printing arrivals");
+    private static final Option departures = new Option("d","get-departures",false, "Printing departures");
+    private static final Option findFlights = new Option("ff", "find-flights",false, "Gets flight by given information");
+    private static final Option checkIn = new Option("ci", "check-in",false, "Allows check-in");
+
+
 
     /**
-     * @param stage
+     *
+     * @param options
+     *
+     */
+    public static void printFormattedOptions(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        PrintWriter printWriter = new PrintWriter(System.out);
+        helpFormatter.printUsage(printWriter, 150, "java -jar projekat-cli-jar-with-dependencies.jar [option] 'something else if needed'");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
+        printWriter.close();
+    }
+
+    public static Options addOptions() {
+        Options options = new Options();
+        options.addOption(arrivals);
+        options.addOption(departures);
+        options.addOption(findFlights);
+        options.addOption(checkIn);
+        return options;
+    }
+
+
+    /**
+     *
+     * @param args
      * @throws Exception
      */
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
-        HomeController homeController = new HomeController();
-        fxmlLoader.setController(homeController);
-        Parent root = fxmlLoader.load();
-        stage.setTitle("App");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
+
+    public static void main(String[] args) throws Exception {
+        Options options = addOptions();
+
+        CommandLineParser commandLineParser = new DefaultParser();
+
+        CommandLine cl = commandLineParser.parse(options, args);
+
+
+        if((cl.hasOption(arrivals.getOpt()) || cl.hasOption(arrivals.getLongOpt())) && cl.hasOption((arrivals.getLongOpt()))){
+            FlightsManager flightsManager = new FlightsManager();
+            List<Flights> flights = new ArrayList<>();
+
+            try {
+                flightsManager.searchArrivals("Sarajevo").forEach(f -> System.out.println(f.getFlight()));
+            }catch(Exception e) {
+                System.out.println("There is no arrivals from this city! Try again.");
+                System.exit(1);
+            }
+        } else if(cl.hasOption(departures.getOpt()) || cl.hasOption(departures.getLongOpt())){
+            FlightsManager flightsManager = new FlightsManager();
+
+            flightsManager.searchDepartures("Sarajevo").forEach(f -> System.out.println(f.getFlight()));
+        } else if(cl.hasOption(findFlights.getOpt()) || cl.hasOption(findFlights.getLongOpt())){
+            FlightsManager flightsManager = new FlightsManager();
+            try {
+                String departure = cl.getArgList().get(0);
+                String arrival = cl.getArgList().get(1);
+                //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy. MM. d.");
+                LocalDate dateOfDeparture = LocalDate.parse(cl.getArgList().get(2));
+
+                Flights flight = new Flights(0, null, departure, arrival, dateOfDeparture);
+                flightsManager.searchFlight(flight).forEach(f -> System.out.println(f.getFlight()));
+            }catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        } else if(cl.hasOption(checkIn.getOpt()) || cl.hasOption(checkIn.getLongOpt())){
+            PersonsManager personsManager = new PersonsManager();
+
+
+
+        } else {
+            printFormattedOptions(options);
+            System.exit(-1);
+//                break;
+        }
+//        }
     }
 }
