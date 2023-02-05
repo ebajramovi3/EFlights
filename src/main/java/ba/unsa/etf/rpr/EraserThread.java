f2f3deb788e2026e1a2f24018331da3d66c51390
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-02-05 14:28:43 +0100

    add classes for password input

---
+++ src/main/java/ba/unsa/etf/rpr/EraserThread.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr;
+
+class EraserThread implements Runnable {
+    private boolean stop;
+
+    public EraserThread(String prompt) {
+        System.out.print(prompt);
+    }
+
+    /**
+     * Begin masking...display asterisks (*)
+     */
+    public void run () {
+        stop = true;
+        while (stop) {
+            System.out.print("\010*");
+            try {
+                Thread.currentThread().sleep(1);
+            } catch(InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Instruct the thread to stop masking
+     */
+    public void stopMasking() {
+        this.stop = false;
+    }
+}
